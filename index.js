3a0f5d822335958506e331f8c5a57d97eeef664d
Vincent Sisk <vinnysisk@gmail.com>
2016-05-23 00:50:54 -0600

    flesh out index.js for testing and remove renamer

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
 const options = require('./options');
+const transformer = require('./transformer');
 
-console.log(options);
+const files = fs.readdirSync(options.directory);
+// console.log(files);
+
+files
+  .filter(value => {
+    return (
+      value.endsWith('.flac') ||
+      value.endsWith('.ogg') ||
+      value.endsWith('.wav'))
+  })
+  .forEach(value => {
+    console.log(value);
+    console.log(transformer(value));
+  });
--- renamer.js
+++
@@ -1,5 +0,0 @@
-const transformer = require('./transformer');
-
-console.log(transformer('01 - Trivium - Ignition.flac'));
-
-
