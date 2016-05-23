00c9601df0542345bdd080bda7ee00de8f72a85f
Vincent Sisk <vinnysisk@gmail.com>
2016-05-23 00:51:33 -0600

    rework transformer to fix bug when song title has a dash

--- transformer.js
+++ transformer.js
@@ -1,15 +1,24 @@
 function transformer (filename) {
-  const re = /^(\d\d)\s-\s(.+)\s-\s(.+)\.(flac|ogg|wav)$/;
+  const re = /^(?:(\d\d)\s-\s)(.+)\.(flac|ogg|wav)$/;
   const result = re.exec(filename);
+  
+  if (!result) {
+    return filename;
+  }
 
   const track = result[1];
-  const artist = result[2];
-  const title = result[3];
-  const type = result[4];
+  const title = getSongTitle(result[2]);
+  const type = result[3];
 
-  console.log(track, artist, title, type);
+  // console.log(`${track}, ${artist}, ${title}, ${type}`);
 
   return `${track} - ${title}.${type}`;
+}
+
+function getSongTitle (artistAndTitle) {
+  return artistAndTitle.split('-').slice(1)
+    .map(str => str.trim())
+    .join(' - ');
 }
 
 module.exports = transformer;
